47d8512f7e59818414b0fdbf1f8e81effa4a1d28
Demone95 <130515586+Demone95@users.noreply.github.com>
2026-07-13 17:51:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C='turni-321-v43',F=['./','index.html','style.css?v=43','script.js?v=43','manifest.json','icon.svg'];
+const C='turni-321-v44',F=['./','index.html','style.css?v=44','script.js?v=44','manifest.json','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
